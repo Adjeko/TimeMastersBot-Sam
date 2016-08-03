f51f23e795259827a2c2642e0e9023076853e2c1
Herbert Pietrzyk <Adjeko@live.de>
2016-08-03 13:34:33 +0200

    migrated views from asp.net webiste deleted webtest project

--- ProjectTimeMasters/TimeMasters.WebCore/Controllers/HomeController.cs
+++ ProjectTimeMasters/TimeMasters.WebCore/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TimeMasters.WebTest.Controllers
+namespace TimeMasters.WebCore.Controllers
 {
     public class HomeController : Controller
     {
--- ProjectTimeMasters/TimeMasters.WebCore/Startup.cs
+++ ProjectTimeMasters/TimeMasters.WebCore/Startup.cs
@@ -7,18 +7,37 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using TimeMasters.WebCore.Models;
 
 namespace TimeMasters.WebCore
 {
     public class Startup
     {
-        
+        public IConfigurationRoot Configuration { get; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+            Configuration = builder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
             services.AddMvc();
 
             services.AddLogging();
@@ -29,7 +48,28 @@ namespace TimeMasters.WebCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseMvcWithDefaultRoute();
+            app.UseApplicationInsightsRequestTelemetry();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             /*loggerFactory.AddConsole();
 
