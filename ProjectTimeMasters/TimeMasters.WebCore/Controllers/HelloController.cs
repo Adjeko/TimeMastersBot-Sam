5b66f72ccabd896ae619c493143cbb223ccdb8ce
Herbert Pietrzyk <Adjeko@live.de>
2016-08-03 14:08:51 +0200

    demonstration of welcome page

--- ProjectTimeMasters/TimeMasters.WebCore/Controllers/HelloController.cs
+++ ProjectTimeMasters/TimeMasters.WebCore/Controllers/HelloController.cs
@@ -8,11 +8,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TimeMasters.WebCore.Controllers
 {
-    [Route("api/[controller]")]
     public class HelloController : Controller
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
             return View();
         }
     }
