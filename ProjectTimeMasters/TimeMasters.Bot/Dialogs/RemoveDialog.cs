d274ae8358eedd5260e2ce451f7ee64bd7d86257
Herbert Pietrzyk <Adjeko@live.de>
2016-09-09 19:27:09 +0200

    dedicated dialogclasses

--- ProjectTimeMasters/TimeMasters.Bot/Dialogs/RemoveDialog.cs
+++ ProjectTimeMasters/TimeMasters.Bot/Dialogs/RemoveDialog.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Web;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using TimeMasters.PortableClassLibrary.Logging;
 
 namespace TimeMasters.Bot.Dialogs
 {
@@ -12,23 +13,43 @@ namespace TimeMasters.Bot.Dialogs
     {
         public async Task StartAsync(IDialogContext context)
         {
+            await context.PostAsync("RemoveDialog StartAsync");
             context.Wait(MessageReceivedAsync);
+
         }
 
         public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
-            PromptDialog.Confirm(
-                context,
-                ConfirmAsync,
-                "Du wolltest etwas löschen?",
-                "Keine Ahnung was du vorhatest.",
-                promptStyle: PromptStyle.None);
+
+            await context.PostAsync("RemoveDialog StartAsync");
+            try
+            {
+                PromptDialog.Confirm(
+                    context,
+                    ConfirmAsync,
+                    "Du wolltest etwas löschen?",
+                    "Keine Ahnung was du vorhatest.",
+                    promptStyle: PromptStyle.None);
+            }
+            catch (System.Exception ex)
+            {
+                Logger.GetInstance().Error<RemoveDialog>("WAS IS DO LOS?", ex);
+            }
+            await context.PostAsync("RemoveDialog StartAsync fertig");
         }
 
         public async Task ConfirmAsync(IDialogContext context, IAwaitable<bool> argument)
         {
-            await context.PostAsync("Danke das du das bestätigt hast");
-            context.Wait(MessageReceivedAsync);
+            var confirm = await argument;
+            if (confirm)
+            {
+                await context.PostAsync("danke für das bestätigen");
+            }
+            else
+            {
+                await context.PostAsync("You just went full retard. Never go full retard.");
+            }
+            context.Done<IMessageActivity>(null);
         }
     }
 }
