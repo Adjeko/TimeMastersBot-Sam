11f1d8b8097cb45ea3b62f65c153d8870992b1ea
Julian <julian_schaefer93@web.de>
2016-09-19 15:25:59 +0200

    Tutorial finished

--- ProjectTimeMasters/TimeMasters.Android/MainActivity.cs
+++ ProjectTimeMasters/TimeMasters.Android/MainActivity.cs
@@ -5,10 +5,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Net;
 
 namespace TimeMasters.Android
 {
-    [Activity(Label = "TimeMasters.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         int count = 1;
@@ -25,8 +26,45 @@ namespace TimeMasters.Android
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
             Button callButton = FindViewById<Button>(Resource.Id.CallButton);
 
+            // Disable the "Call" button
+            callButton.Enabled = false;
 
-           
+            // Add code to translate number
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (String.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                // On "Call" button click, try to dial phone number.
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate {
+                    // Create intent to dial phone
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(global::Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // Show the alert dialog to the user and wait for response.
+                callDialog.Show();
+            };
+
         }
     }
 }
