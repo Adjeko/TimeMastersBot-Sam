ad799c6f1f8b873c0c95e0cbc902f3b0cc390953
Roland Pietrzyk <Roland.Pietrzyk@gmail.com>
2016-10-01 21:38:42 +0200

    Connection Closing Exception

---
+++ ProjectTimeMasters/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeMastersClassLibrary.Database;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseClient DBC = new DatabaseClient();
+            DBC.Insert();
+            Console.WriteLine("Läuft");
+            Console.ReadLine();
+        }
+    }
+}
