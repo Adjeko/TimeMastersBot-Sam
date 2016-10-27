e5f9af71b2e19e01e0cbc2da37125d5ef85aadc5
Herbert Pietrzyk <Adjeko@live.de>
2016-10-27 21:33:18 +0200

    created help and register dialog

---
+++ ProjectTimeMasters/TimeMasters.Bot/Dialogs/HelpDialog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace TimeMasters.Bot.Dialogs
+{
+    public class HelpDialog : LuisDialog<object>
+    {
+    }
+}
---
+++ ProjectTimeMasters/TimeMasters.Bot/Dialogs/RegisterDialog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace TimeMasters.Bot.Dialogs
+{
+    public class RegisterDialog : LuisDialog<object>
+    {
+
+        public RegisterDialog()
+        {
+            
+        }
+    }
+}
